bc2b3084b83fd5db29bb60bd0d966fe6242abda8
maya <maya.e@webased.net>
2024-10-31 01:09:42 -0700

    nas api config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 
 function App() {
+
+  const [data , setData] = useState(null);
+  const [ loading , setLoading] = useState(null)
+
+  useEffect(()=>{
+  
+    async function fetAPIData(){
+
+      const NAST_KEY = import.meta.env.VITE_NASA_API_KEY
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NAST_KEY}`
+      console.log(url)
+
+      try{
+       const res =  await fetch(url);
+       const apiData = await res.json();
+       setData(apiData)
+
+      }catch(err){
+        console.log(err.message)
+      }
+      
+    }
+    fetAPIData();
+  },[])
 
   const [showModal , setShowModal] = useState(false);
   const handleToggleModal = ()=>{
@@ -13,7 +37,7 @@ function App() {
   return (
     <>
        {showModal && (<SideBar handleToggleModal={handleToggleModal} />)}
-       <Main />
+       {data ? (<Main />) : (<div className="loadingState"> <i className="fa-solid fa-gear"></i></div>)}
        <Footer handleToggleModal={handleToggleModal}/>
     </>
   )
